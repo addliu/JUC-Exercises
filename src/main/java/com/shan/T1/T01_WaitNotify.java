e698a6e6454d94330f3949ce8e7fce0d26bd7b6a
addliu <pyliuchuang@vip.qq.com>
2020-05-22 11:10:59 +0800

    优化wait、notify

--- src/main/java/com/shan/T1/T01_WaitNotify.java
+++ src/main/java/com/shan/T1/T01_WaitNotify.java
@@ -1,5 +1,7 @@
 package com.shan.T1;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 使用Synchronized锁与wait、notify来完成
  */
@@ -39,7 +41,12 @@ public class T01_WaitNotify {
                 }
             }
         });
-
+        // 需让t1线程先执行，t2线程后执行
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         t2.start();
 
     }
